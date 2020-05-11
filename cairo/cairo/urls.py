a678f6ac890fdffa87c902c27998befb568c3a4f
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-05-11 21:40:46 +0530

    getting started

--- cairo/cairo/urls.py
+++ cairo/cairo/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(' ',include(('shop.urls','shop'),namespace='shop')),
+    path('shop/',include(('shop.urls','shop'),namespace='shop')),
 ]
--- cairo/shop/views.py
+++ cairo/shop/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request,)
-
+    return render(request, 'shop/homepage.html', context=None)
