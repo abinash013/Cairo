c028e7e71a0bc2d1cf61f2eb93df48e94cc83ee7
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-05-16 16:40:41 +0530

    backend

--- cairo/shop/models.py
+++ cairo/shop/models.py
@@ -4,16 +4,16 @@ import datetime
 # Create your models here.
 class department(models.Model):
     department_name = models.CharField(max_length=20)
-    department_description=models.CharField(max_length=400)
+    #department_description=models.CharField(max_length=400)
     def __str__(self):
         return self.department_name
 
 class item(models.Model):
     department = models.ForeignKey(department,on_delete=models.CASCADE)
-    item_name = models.CharField(max_length=20)
+    item_name = models.CharField(max_length=100)
     date_added = models.DateTimeField(auto_now_add=True)
     price = models.IntegerField()
-    image=models.CharField(max_length=40)
-    description = models.CharField(max_length=600)
+    image=models.CharField(max_length=100)
+    description = models.CharField(max_length=2600)
     def __str__(self):
         return self.item_name
--- cairo/shop/views.py
+++ cairo/shop/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import department,item
 
 # Create your views here.
 def home(request):
     return render(request, 'shop/homepage.html', context=None)
+
+def index(request,dname):
+    try:
+        dept=department.objects.get(department_name=dname)
+    except department.DoesNotExist:
+        dept=None
+    return render(request,"shop/index.html",{'dept':dept})
