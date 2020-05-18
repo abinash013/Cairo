229f90007a975964e6e6ab514eb73d8e276c7159
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-05-18 16:12:58 +0530

    .

--- cairo/shop/urls.py
+++ cairo/shop/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('home', views.home, name="homepage"),
     path('index/<str:dname>', views.index, name="index"),
+    path('signin', views.signin, name="signin"),
 ]
--- cairo/shop/views.py
+++ cairo/shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import department,item
+from .models import department, item
 
 # Create your views here.
 def home(request):
@@ -7,7 +7,10 @@ def home(request):
 
 def index(request,dname):
     try:
-        dept=department.objects.get(department_name=dname)
+        dept = department.objects.get(department_name=dname)
     except department.DoesNotExist:
-        dept=None
+        dept = None
     return render(request,"shop/index.html",{'dept':dept})
+
+def signin(request):
+    return render(request, 'shop/signin.html', context=None)
