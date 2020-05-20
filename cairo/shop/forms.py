daa239b22dc4ff129c6a07c5e46f773c4cfc1d7c
abinash013 <45894844+abinash013@users.noreply.github.com>
2020-05-20 18:03:26 +0530

    Login
    Login verification done and dusted

--- cairo/shop/forms.py
+++ cairo/shop/forms.py
@@ -8,3 +8,7 @@ class signinform(forms.ModelForm):
         widgets = {
         'password': forms.PasswordInput(),
     }
+
+class loginform(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField(widget = forms.PasswordInput())
--- cairo/shop/views.py
+++ cairo/shop/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse
 from .models import department, details
-from .forms import signinform
+from .forms import signinform,loginform
 # Create your views here.
 def home(request):
     return render(request, 'shop/homepage.html', context=None)
@@ -19,12 +19,28 @@ def siginprocess(request):
     if (request.method=="POST"):
         currentform = signinform(request.POST)
         if currentform.is_valid():
+            uobj = currentform.cleaned_data
+            uemail = uobj["email"]
+            if(details.objects.filter(email=uemail).exists()):
+                error_message = True
+                return render(request, 'shop/signin.html', {'error_message': error_message})
             currentform.save()
             return render(request, 'shop/homepage.html', context=None)
     else:
         currentform = signinform()
-    return render(request, 'shop/signin.html', {'currentform':currentform})
+    return render(request, 'shop/signin.html', {'currentform': currentform})
 
 
 def login(request):
-    return render(request,'shop/homepage.html')
+    if(request.method=="POST"):
+        formInstance = loginform(request.POST)
+        if formInstance.is_valid():
+            userobj = formInstance.cleaned_data
+            uemail = userobj["email"]
+            upassword = userobj["password"]
+            d = details.objects.get(email=uemail)
+            if ((details.objects.filter(email=uemail).exists()) and (d.password == upassword)):
+                return render(request, 'shop/signin.html')
+            else:
+                error_message=True
+                return render(request, 'shop/homepage.html', {'error_message':error_message})
